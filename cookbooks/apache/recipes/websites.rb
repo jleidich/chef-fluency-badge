7564739cb32a98a96618d743440a502482552685
jason <jason.leidich@gmail.com>
2018-06-14 16:18:19 -0400

    just cause

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -2,3 +2,11 @@ file 'default www' do
 	path '/var/www/html/index.html'
 	content 'Hello World!'
 end
+
+webnodes = search('node', 'role:web')
+
+# ruby syntax loop
+
+webnodes.each do |node|
+	puts node
+end
