685408853dc66b7e490b7bb8b897af26e3f763e5
jason <jason.leidich@gmail.com>
2018-06-14 16:23:18 -0400

    updated apache to v2.0

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,12 +1,12 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'Hello World!'
+	content 'Hello World! Version 2.0'
 end
 
 webnodes = search('node', 'role:web')
 
 # ruby syntax loop
 
-webnodes.each do |node|
-	puts node
-end
+#webnodes.each do |node|
+#	puts node
+#end
